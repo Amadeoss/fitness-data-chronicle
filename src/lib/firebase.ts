9ba7658cc82423cb34eb28208086c779f5afe5bd
Matteo Italiano <matteoitalianoistcool@gmail.com>
2025-05-11 00:36:31 +0200

    API Keys changed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase-Konfiguration
 const firebaseConfig = {
-  // Diese Werte müssen mit deinen echten Firebase-Konfigurationswerten ersetzt werden
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDban0wodngF4qphBgcySvjJlUI_0LcSDc",
+  authDomain: "fitness-app-f2a07.firebaseapp.com",
+  projectId: "fitness-app-f2a07",
+  storageBucket: "fitness-app-f2a07.firebasestorage.app",
+  messagingSenderId: "740705828464",
+  appId: "1:740705828464:web:92ad24a7fa0d4c7c143eb5",
+  measurementId: "G-N7L61LQ1BN"
 };
+
 
 // Firebase initialisieren
 const app = initializeApp(firebaseConfig);
